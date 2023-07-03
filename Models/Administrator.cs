de82bc6aef45dbc00a2fbfaed52fc28a72bf2458
Mizerael <dan-acer@outlook.com>
2023-07-03 17:27:00 +0400

    check  gpg

--- Models/Administrator.cs
+++ Models/Administrator.cs
@@ -5,5 +5,6 @@ public class Administartor
     public long Id { get; set; }
     public string LogCode { get; set; }
     public string PassCode { get; set; }
+    
     public int AccessLevel { get; set; }
 }
